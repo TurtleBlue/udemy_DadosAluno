572bb2982dc7305f54e7a7e9183e06ae8acbebe6
Vinicius Hideaki Menda Nakano <vini.ttb@hotmail.com>
2024-04-01 17:48:20 -0300

    Add project files.

---
+++ udemy_DadosAluno/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy_DadosAluno
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double notaFinal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public bool aprovacao()
+        {
+            if ((nota1 + nota2 + nota3) < 60)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public double pontosFaltantes()
+        {
+            return 60 - (nota1 + nota2 + nota3);
+        }
+    }
+}
---
+++ udemy_DadosAluno/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace udemy_DadosAluno
+{
+    internal class Program
+    {
+        /* Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres
+         * do ano (primeiro trimestre vale 30 e o segundo valem 35 cada). Ao final, mostrar qual a nota do aluno
+         * no ano. Dizer também se o aluno está aprovado ou reprovado e, em caso negativo, quantos pontos faltam
+         * para o aluno obter o mínimo para ser aprovado (60 pontos). Criar uma calsse aluno para resolver o problema.
+         */
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno:");
+            aluno.nome = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"Certo, agora digite as três notas do {aluno.nome}:");
+            aluno.nota1 = double.Parse(Console.ReadLine());
+            aluno.nota2 = double.Parse(Console.ReadLine());
+            aluno.nota3 = double.Parse(Console.ReadLine());
+
+            if (aluno.aprovacao() == false)
+            {
+                Console.WriteLine($"O aluno {aluno.nome} está reprovado.\n" +
+                    $"A nota final foi de: {aluno.notaFinal():n2}\n" +
+                    $"A quantidade de pontos faltantes foi de: {aluno.pontosFaltantes():n2}");
+            }
+            else
+            {
+                Console.WriteLine($"O aluno {aluno.nome} foi aprovado. \n" +
+                    $"A nota final foi de: {aluno.notaFinal():n2}");
+            }
+        }
+    }
+}
